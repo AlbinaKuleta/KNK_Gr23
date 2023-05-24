e22e5d25cac6ee122a929d71cd99011aec0090d4
Dominik <130363380+dominikpllashniku@users.noreply.github.com>
2023-05-24 22:40:16 +0200

    Create getData.java

---
+++ Projekti_vs/src/getData.java
@@ -0,0 +1,13 @@
+package studentmanagementsystem;
+
+/**
+ *
+ * @author WINDOWS 10
+ * SUBSCRIBE OUR YOUTUBE CHANNEL -> https://www.youtube.com/channel/UCPgcmw0LXToDn49akUEJBkQ
+ * THANKS FOR YOUR SUPPORT : ) 
+ */
+public class getData {
+    
+    public static String username;
+    public static String path;
+    
