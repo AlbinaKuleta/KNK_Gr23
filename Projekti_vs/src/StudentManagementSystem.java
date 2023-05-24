e34002ef963da858a6fb962d88557bf06a4c4d3e
Ajshe <115940306+ajsheselmani@users.noreply.github.com>
2023-05-25 00:07:03 +0200

    ...

--- Projekti_vs/src/StudentManagementSystem.java
+++ Projekti_vs/src/StudentManagementSystem.java
@@ -1,1 +1,29 @@
 
+package studentmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class StudentManagementSystem extends Application {
+    
+    private double x = 0 ;
+    private double y = 0;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
