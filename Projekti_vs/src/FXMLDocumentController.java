c13bda84896cda397d405953122b904a03563aec
Ajshe <115940306+ajsheselmani@users.noreply.github.com>
2023-05-24 23:56:05 +0200

    FXMLDocumentController.java
    added code

--- Projekti_vs/src/FXMLDocumentController.java
+++ Projekti_vs/src/FXMLDocumentController.java
@@ -1,1 +1,66 @@
 
+package studentmanagementsystem;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Button close;
+    
+//    DATABASE TOOls
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+    
+// OUR DATABASE  
+    
+    private double x= 0 ;
+    private double y= 0;
+    
+    public void loginAdmin(){
+        
+        String sql = "SELECT * FROM admin WHERE username = ? and password = ?";
+        
+        connect = database.connectDb();
+        
+        try{ 
+            Alert alert;
+            
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, username.getText());
+            prepare.setString(2, password.getText());
+            
+            result = prepare.executeQuery();
